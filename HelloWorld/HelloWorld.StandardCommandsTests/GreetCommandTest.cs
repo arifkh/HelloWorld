583e0f7f287a2a59d8bec32b0fc0b49603e333a2
Arif Khan <arifkhan1@gmail.com>
2019-01-20 01:10:17 -0600

    Unit test added

---
+++ HelloWorld/HelloWorld.StandardCommandsTests/GreetCommandTest.cs
@@ -0,0 +1,25 @@
+﻿using HelloWorld.Core;
+using HelloWorld.StandardCommands;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace HelloWorld.StandardCommandsTests
+{
+    [TestClass]
+    public class GreetCommandTest
+    {
+        [TestMethod]
+        public void WriteMessageCalledOneTime()
+        {
+            // Arrange
+            var messageRepositoryMock = new Mock<IMessageRepository>();
+            ICommand greetCommand = new GreetCommand(messageRepositoryMock.Object);
+
+            // Act
+            greetCommand.RunCommand();
+
+            // Assert
+            messageRepositoryMock.Verify(x => x.WriteMessage(It.IsAny<string>()), Times.Exactly(1));
+        }
+    }
+}
